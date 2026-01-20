e5017fbf5cf3c488bd88b5ac0f7c3dc458df5b5c
Jay Lee <leejayjong@gmail.com>
2026-01-20 11:26:24 -0800

    feat: create footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        SpringK Art
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function Footer() {
+  return (
+    <Box component="footer" sx={{ bgcolor: 'background.paper', py: 6, mt: 'auto' }}>
+      <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom>
+          SpringK
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Capturing moments in colors.
+        </Typography>
+        <Copyright />
+      </Container>
+    </Box>
+  );
+}
