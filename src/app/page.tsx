e6b288500cbe0b04503f419ed5ca89a63627b031
Jay Lee <randomdori@gmail.com>
2026-05-06 10:31:46 -0700

    remove art work from main page metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import type { Metadata } from 'next';
 import HomeClient from '@/components/Home/HomeClient';
 
 export const metadata: Metadata = {
-  title: 'Art Work | Spring Days',
+  title: 'Spring Days',
   description: 'Explore the diverse collection of artworks by SpringK, featuring themes of emotions, recovery, and bright colors.',
   openGraph: {
-    title: 'Art Work | Spring Days',
+    title: 'Spring Days',
     description: 'Explore the diverse collection of artworks by SpringK, featuring themes of emotions, recovery, and bright colors.',
   },
 };
